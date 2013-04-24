9e937fe803ee2a10b392aeec91da7471df1e344d
dam7633 <dam7633@rit.edu>
2013-04-24 11:32:47 -0400

    Adding cookie discovery

--- src/hot/fuzzer/Fuzzer.java
+++ src/hot/fuzzer/Fuzzer.java
@@ -4,20 +4,25 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.util.Cookie;
 
 public class Fuzzer {
 	private Date lastRequestDate;
 	private long minimumRequestInterval;
 	private WebClient webClient;
+	private HashSet<Cookie> discoveredCookies;
 	
 	Fuzzer() {
 		// Initialization.
 		lastRequestDate = null;
 		minimumRequestInterval = 0;
+		discoveredCookies = new HashSet<Cookie>();
 		webClient = new WebClient();
 		
 		// Configure the web client.
@@ -30,12 +35,25 @@ public class Fuzzer {
 	
 	public HtmlPage getPage(String url) throws IOException, MalformedURLException {
 		waitForMinimumRequestTimeout();
-		return webClient.getPage(url);
+		HtmlPage page = webClient.getPage(url);
+		discoverCookies();
+		return page;
 	}
 	
 	public HtmlPage getPage(URL url) throws IOException, MalformedURLException {
 		waitForMinimumRequestTimeout();
-		return webClient.getPage(url);
+		HtmlPage page = webClient.getPage(url);
+		discoverCookies();
+		return page;
+	}
+	
+	public void printReport() {
+		System.out.println("==================================================");
+		System.out.println("All discovered cookies:");
+		for (Cookie cookie : discoveredCookies) {
+			System.out.println(cookie.toString());
+		}
+		System.out.println("==================================================");
 	}
 
 	public long getMinimumRequestInterval() {
@@ -60,8 +78,17 @@ public class Fuzzer {
 		lastRequestDate = new Date();
 	}
 	
+	private void discoverCookies() {
+		Set<Cookie> cookies = webClient.getCookieManager().getCookies();
+		if (cookies != null && cookies.size() > 0) {
+			discoveredCookies.addAll(cookies);
+		}
+	}
+	
 	public static void main(String[] args) throws FailingHttpStatusCodeException, MalformedURLException, IOException {
 		Fuzzer theFuzz = new Fuzzer();
+		//theFuzz.getPage("http://www.google.com/");
+		theFuzz.printReport();
 		theFuzz.close();
 	}
 }
