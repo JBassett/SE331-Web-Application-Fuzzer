539b97421774ef45c47002383ae492dff3515863
Justin Bassett <Bassett.JustinT@gmail.com>
2013-04-24 11:41:04 -0400

    Add in a find all links on page.

--- src/hot/fuzzer/Fuzzer.java
+++ src/hot/fuzzer/Fuzzer.java
@@ -3,10 +3,13 @@ package hot.fuzzer;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
 public class Fuzzer {
@@ -58,6 +61,29 @@ public class Fuzzer {
 			}
 		}
 		lastRequestDate = new Date();
+	}
+	
+	/**
+	 * This method will find all the links on the given page that are part of
+	 * the server.
+	 * 
+	 * @param page
+	 *            The page to search
+	 * @return A list of all possible links on the server
+	 */
+	public List<URL> getLinks(HtmlPage page){
+		List<URL> retVal = new ArrayList<URL>();
+		
+		for (HtmlAnchor a : page.getAnchors()) {
+			try {
+				if(a.getHrefAttribute().startsWith("/"))
+					retVal.add(new URL(page.getUrl() + (a.getHrefAttribute().substring(1))));
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return retVal;
 	}
 	
 	public static void main(String[] args) throws FailingHttpStatusCodeException, MalformedURLException, IOException {
